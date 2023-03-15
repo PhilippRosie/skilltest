0269ba05a41e92a5cf8cf97a80aa1d287296164c
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-03-15 11:02:26 +0100

    added media to section1, also added javascript to the navbar för smaller screens

---
+++ styles/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menu_btn = document.querySelector('.burger-menu');
+    const mobile_menu = document.querySelector('.mobile-nav');
+    
+        menu_btn.addEventListener('click', function () {
+            menu_btn.classList.toggle('is-active');
+            mobile_menu.classList.toggle('is-active');
+        });
+    });
